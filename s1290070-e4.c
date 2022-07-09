a37a2a382b0bdf867aaef47a71be95405f7d7307
Yuma22611 <108879864+Yuma22611@users.noreply.github.com>
2022-07-09 17:51:23 +0900

    HoT: initial revision

---
+++ s1290070-e4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+    int res[3];
+    int cnthead=0, cnttails=0;
+    char name[15];
+
+    printf("Who are you?\n> ");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+
+    srand(time(NULL));
+    printf("Tossing a coin...\n");
+    for(int i=0; i<3; i++) {
+        printf("Round %d: ",i+1);
+        res[i] = rand();
+        if(res[i] % 2 == 1) {
+            printf("Heads\n");
+            cnthead++;
+        }
+        else {
+            printf("Tails\n");
+            cnttails++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", cnthead, cnttails);
+
+    if(cnthead > cnttails) printf("%s won!\n", name);
+    else printf("%s lost.\n",name);
+
+    return 0;
+}
